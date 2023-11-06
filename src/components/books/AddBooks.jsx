aefef34edcf3d844791b6a0126ce08a9371e8e94
ZaibLComrade <kmjahanzaib@gmail.com>
2023-11-06 10:37:36 +0600

    Modified categories field in add book page

--- src/components/books/AddBooks.jsx
+++ src/components/books/AddBooks.jsx
@@ -1,10 +1,17 @@
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { useForm } from "react-hook-form";
+import {useEffect, useState} from "react";
 
 export default function AddProduct() {
+	const [categories, setCategories] = useState([])
 	const axiosSecure = useAxiosSecure();
 	const { register, handleSubmit, formState: { errors } } = useForm();
+	
+	useEffect(() => {
+		axiosSecure.get("/books/categories")
+			.then(res => setCategories(res.data));
+	}, [axiosSecure])
 	
 	const onSubmit = newBook => {
 		axiosSecure.post("/books", newBook)
@@ -82,17 +89,9 @@ export default function AddProduct() {
 							<span className="label-text" required>Category</span>
 						</label>
 						<select {...register("category")} className="input-bordered input">
-							<option value="0">0</option>
-							<option value="0.5">0.5</option>
-							<option value="1.0">1.0</option>
-							<option value="1.5">1.5</option>
-							<option value="2.0">2.0</option>
-							<option value="2.5">2.5</option>
-							<option value="3.0">3.0</option>
-							<option value="3.5">3.5</option>
-							<option value="4.0">4.0</option>
-							<option value="4.5">4.5</option>
-							<option value="5.0">5.0</option>
+							{
+								categories.map(categ => <option key={categ._id} value={ categ.id }>{categ.category}</option>)
+							}
 						</select>
 					</div>
 					
@@ -116,7 +115,7 @@ export default function AddProduct() {
 				{errors.exampleRequired && <span>This field is required</span>}
 				{/* Submit */}
 				<div className="mx-auto mt-6 w-max col-span-1 md:col-span-2">
-					<input type="submit" value="Add Product" className="w-full btn hover:bg-custom-white-2 border-custom-coffee-2 bg-custom-coffee-1 input-bordered"/>
+					<input type="submit" value="Add Book" className="w-full btn hover:bg-custom-white-2 border-custom-coffee-2 bg-custom-coffee-1 input-bordered"/>
 				</div>
 			</form>
 		</div>

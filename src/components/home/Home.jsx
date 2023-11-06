e4f4015f4c0d5b81aabe747c298a18404f374ae8
ZaibLComrade <kmjahanzaib@gmail.com>
2023-11-06 12:46:38 +0600

    Added Subscribe section to home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
 import Categories from "./Categories";
+import Subscribe from "./Subscribe";
 
 export default function Home() {
 	return <div>
 		<Banner/>
 		<Categories/>
+		<Subscribe/>
 	</div>
 }
 
---
+++ src/components/home/Subscribe.jsx
@@ -0,0 +1,20 @@
+export default function Subscribe() {
+	return <div>
+		<div className="relative flex flex-col items-center justify-center min-h-screen p-8 overflow-hidden sm:p-12">
+			<div className="absolute w-full h-full">
+				<img className="object-cover w-full h-full" src="https://i.postimg.cc/bNnQLpmq/newsletter-min.jpg"/>
+			</div>
+			<h1 className="absolute z-10 text-5xl text-black lg:text-7xl top-20 font-playfair">Subscribe</h1>
+			<div className="z-10 w-full max-w-4xl border-2 border-black bg-black/60 rounded-md p-14">
+				<div className="flex flex-col items-center">
+					<span className="px-2 py-px font-bold text-yellow-800 bg-yellow-100 rounded-lg -rotate-1 font-dancing">More than 1600 subscribers join us daily</span>
+					<h3 className="max-w-2xl mt-2 text-2xl font-bold leading-tight text-center text-white sm:text-3xl md:text-4xl font-lato md:leading-tight">Subscribe to our newsletter to keep getting latest books updates</h3>
+					<form action="" className="flex flex-col items-center w-full max-w-md mx-auto mt-4 gap-3 sm:flex-row sm:gap-0">
+						<input type="email" name="email" id="email" className="px-0 py-3 border-2 border-gray-300 rounded md:px-4 grow focus:border-emerald-500 focus:outline-none sm:rounded-l-md sm:rounded-r-none sm:border-r-0" placeholder="Email Address" />
+						<button type="submit" className="px-5 py-4 font-bold text-white normal-case rounded font-montserrat border-emerald-500 bg-emerald-500 btn sm:rounded-l-none sm:rounded-r-md">Subscribe</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+}
